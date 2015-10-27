c689342f1c1874cfa04e0cac31f2528763fac9b9
tkqubo <tk.qubo@gmail.com>
2015-10-27 19:10:30 +0900

    test: add bootstrap

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+'use strict';
+export function hello(): string {
+  'use strict';
+  return 'world';
+}
---
+++ test/index.spec.ts
@@ -0,0 +1,5 @@
+import assert from 'power-assert';
+import { hello } from '../src/index';
+
+assert(hello() === 'world');
+
