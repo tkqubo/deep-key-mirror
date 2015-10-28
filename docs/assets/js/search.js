e0885408c1d75329fbf151ae1c00793fdbbfd43e
tkqubo <tk.qubo@gmail.com>
2015-10-29 00:03:03 +0900

    doc: update

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -1,1 +1,1 @@
-﻿var typedoc = typedoc || {};typedoc.search = typedoc.search || {};typedoc.search.data = {"kinds":{},"rows":[]};
+﻿var typedoc = typedoc || {};typedoc.search = typedoc.search || {};typedoc.search.data = {"kinds":{"1":"External module","64":"Function"},"rows":[{"id":0,"kind":1,"name":"\"deep-key-mirror\"","url":"modules/_deep_key_mirror_.html","classes":"tsd-kind-external-module"},{"id":1,"kind":64,"name":"deepKeyMirror","url":"modules/_deep_key_mirror_.html#deepkeymirror","classes":"tsd-kind-function tsd-parent-kind-external-module","parent":"\"deep-key-mirror\""}]};
