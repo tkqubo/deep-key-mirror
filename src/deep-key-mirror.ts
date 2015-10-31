1ca4c9446f6f928ec999304fb4c23b2d38985651
tkqubo <tk.qubo@gmail.com>
2015-10-31 22:57:01 +0900

    test: format

--- src/deep-key-mirror.ts
+++ src/deep-key-mirror.ts
@@ -1,13 +1,22 @@
 'use strict';
-import * as _ from 'lodash';
 
 export function deepKeyMirror(obj: any): any {
   'use strict';
-  if (_.isObject(obj)) {
+  if (obj === null || obj === undefined) {
     return obj;
   }
-  if (_.isArray(obj)) {
+  if (['boolean', 'number', 'string', 'function'].indexOf(typeof obj) !== -1) {
     return obj;
   }
+  if (obj instanceof Array) {
+    return obj;
+  }
+
+  Object.keys(obj)
+    .filter((prop: string) => {
+      let value = obj[prop];
+      return value === null || value === undefined;
+    })
+    .forEach((prop: string) => obj[prop] = prop);
   return obj;
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,15 +3,25 @@ import assert from 'power-assert';
 import { deepKeyMirror } from '../src/index';
 
 // not an object nor an array
-{
-  let obj = {};
-  assert(deepKeyMirror(obj) === obj);
-  let num = 42;
-  assert(deepKeyMirror(num) === num);
-  let str = 'foo';
-  assert(deepKeyMirror(str) === str);
-  let bool = true;
-  assert(deepKeyMirror(bool) === bool);
-  let func = () => 3;
-  assert(deepKeyMirror(func) === func);
-}
+describe('deepKeyMirror', () => {
+  it('does nothing when an empty object is passed as an argument', () => {
+    let obj = {};
+    assert(deepKeyMirror(obj) === obj);
+  });
+  it('does nothing when a number is passed as an argument', () => {
+    let num = 42;
+    assert(deepKeyMirror(num) === num);
+  });
+  it('does nothing when a string is passed as an argument', () => {
+    let str = 'foo';
+    assert(deepKeyMirror(str) === str);
+  });
+  it('does nothing when a boolean is passed as an argument', () => {
+    let bool = true;
+    assert(deepKeyMirror(bool) === bool);
+  });
+  it('does nothing when a function is passed as an argument', () => {
+    let func = () => 3;
+    assert(deepKeyMirror(func) === func);
+  });
+});
