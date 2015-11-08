068f47b64c93bbee2814f2fd070a8d4ddabf9f5a
tkqubo <tk.qubo@gmail.com>
2015-11-08 19:49:18 +0900

    feat: implement deep mirroring

--- src/deep-key-mirror.ts
+++ src/deep-key-mirror.ts
@@ -3,6 +3,11 @@ import * as _ from 'lodash';
 
 export function deepKeyMirror(obj: any): any {
   'use strict';
+  return doDeepKeyMirror(obj);
+}
+
+function doDeepKeyMirror(obj: any, paths: string[] = []): any {
+  'use strict';
   if (obj === null || obj === undefined) {
     return obj;
   }
@@ -15,17 +20,28 @@ export function deepKeyMirror(obj: any): any {
       obj,
       (mirrored: any, prop: any) =>
         Object.defineProperty(mirrored, prop, {
-          value: prop.toString(),
+          value: buildValue(...paths, prop.toString()),
+          enumerable: true,
           writable: true,
           configurable: true
         }),
       {} as any);
   }
 
-  Object.keys(obj)
-    .filter((prop: string) => {
-      return obj.hasOwnProperty(prop) && (obj[prop] === null || obj[prop] === undefined);
-    })
-    .forEach((prop: string) => obj[prop] = prop);
+  let properties: string[] = Object
+    .keys(obj)
+    .filter((prop: string) => obj.hasOwnProperty(prop));
+  let [emptyProps, nonEmptyProps] = _.partition(properties, (prop: string) => obj[prop] === null || obj[prop] === undefined);
+  // assign prop name if its value is null or undefined
+  emptyProps
+    .forEach((prop: string) => obj[prop] = buildValue(...paths, prop));
+  // assign recursively prop if its value is not null nor undefined
+  nonEmptyProps
+    .forEach((prop: string) => obj[prop] = doDeepKeyMirror(obj[prop], [...paths, prop]));
   return obj;
+}
+
+function buildValue(...paths: string[]): string {
+  'use strict';
+  return paths.join('.');
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -11,27 +11,43 @@ describe('deepKeyMirror', () => {
     let uv: any = undefined;
     assert(deepKeyMirror(uv) === undefined);
   });
+
   it('does nothing when an empty object is passed as an argument', () => {
     let obj = {};
     assert(deepKeyMirror(obj) === obj);
   });
+
   it('does nothing when a number is passed as an argument', () => {
     let num = 42;
     assert(deepKeyMirror(num) === num);
   });
+
   it('does nothing when a string is passed as an argument', () => {
     let str = 'foo';
     assert(deepKeyMirror(str) === str);
   });
+
   it('does nothing when a boolean is passed as an argument', () => {
     let bool = true;
     assert(deepKeyMirror(bool) === bool);
   });
+
   it('does nothing when a function is passed as an argument', () => {
     let func = () => 3;
     assert(deepKeyMirror(func) === func);
   });
-  describe('with an object which should be processed', () => {
+
+  describe('with an array', () => {
+    it('supplies mirrored object with its key taken from array element', () => {
+      let array: (string|number)[] = ['foo', 'bar', 24];
+      let mirrored = deepKeyMirror(array);
+      assert(mirrored.foo === 'foo');
+      assert(mirrored.bar === 'bar');
+      assert(mirrored[24] === '24');
+    });
+  });
+
+  describe('with a flat object', () => {
     it('supplies key name itself to its value', () => {
       let obj: any = { name: null, action: undefined };
       assert(deepKeyMirror(obj).name === 'name');
@@ -44,13 +60,46 @@ describe('deepKeyMirror', () => {
     });
   });
 
-  describe('with an array', () => {
-    it('supplies mirrored object with its key taken from array element', () => {
-      let array: (string|number)[] = ['foo', 'bar', 24];
-      let mirrored = deepKeyMirror(array);
-      assert(mirrored.foo === 'foo');
-      assert(mirrored.bar === 'bar');
-      assert(mirrored[24] === '24');
+  describe('with a nested object', () => {
+    it('supplies path-concatenated prop name to its value', () => {
+      let actual = deepKeyMirror({
+        user: {
+          created: null,
+          updated: null,
+          deleted: null
+        },
+        status: [
+          'start', 'stop'
+        ],
+        other: {
+          fixed: 'FIXED_VALUE',
+          misc: {
+            miscA: null,
+            miscB: undefined,
+            miscZ: 'Z'
+          }
+        }
+      });
+      let expected: any = {
+        user: {
+          created: 'user.created',
+          updated: 'user.updated',
+          deleted: 'user.deleted'
+        },
+        status: {
+          start: 'status.start',
+          stop: 'status.stop'
+        },
+        other: {
+          fixed: 'FIXED_VALUE',
+          misc: {
+            miscA: 'other.misc.miscA',
+            miscB: 'other.misc.miscB',
+            miscZ: 'Z'
+          }
+        }
+      };
+      assert.deepEqual(actual, expected);
     });
   });
 });
